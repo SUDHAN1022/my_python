354c70bae7a6c57cb6abfa249efc0cc4e2d07c32
HARIHARASUDHAN M <98317148+SUDHAN1022@users.noreply.github.com>
2022-11-12 22:51:46 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,184 @@
+#x= 5
+#print(type(5))
+
+ #str1="hello world"
+#str2="computerscience"
+#print(str1[0])
+#print(str2[8])
+
+#str="cricket"
+#a=str.replace("t","ter")
+#print("Result:",a)
+
+#splitstr="word1,word2,word3"
+# a = splitstr.split(",")
+ #print("Result :", a);
+
+#str = "you can do it man"
+#d= str.count("a")
+#print(d)
+
+#str ="8KA3U3@*(#mf"
+#a=min(str)
+#print(a)
+
+#x= True
+# #print(type(x))
+
+#a=5.5
+#b=int(a)
+#print(b)
+
+#int(True)
+#int(False)
+#a=10
+#b=6
+#c=complex(a,b)
+#print(c)
+
+#x=4
+#x=""
+#print(x)
+
+#x,y,z =" apple","orange","cherry"
+#print(x,y,z)
+
+#x=5
+#y=3
+#print(x+y)
+
+#n=int(input())
+#if (n%2!=0) :
+#    print("Weird")
+#elif (n%2==0) and range(2,6):
+   # print("Not Weird")    
+#elif (n%2==0) and n>20:
+#    print("Not Weird")
+
+#list is a collection - ordered and cahnageable
+
+#x= ["apple","orange","cherry"]
+#print(x)
+#print(type(x))
+
+#nums=[10,20,30,40,50]
+#print(nums[:-2])
+
+#values=["hari",20,57.8]
+#print(values)
+
+#num = [1,2,3,4,5]
+#value=["hari",40,3.9]
+#mullist=[num,value]
+#print(mullist)
+
+#num = [1,2,3,4,5]
+#value=["hari",40,3.9]
+#mullist=[num,value]
+#mullist.append(10)
+#print(mullist)
+
+#num=[1,2,3,4]
+#num.remove(2)
+#print(num)
+
+#num=[1,2,3,4,5]
+#num.pop()
+#print(num)
+
+
+#num=[1,2,3,4,5,6]
+#del num[2:]
+#print(num)
+
+#num=[1,2,3,4,5]
+#num.extend([6,7,8,9,10])
+#print(num)
+
+#num=[1,2,3,4,5]
+#a=sum(num)
+#print(a)
+
+#num=[10,56,22,1,78]
+#a=sorted(num)
+#print(a)
+
+#tuple
+
+#x= ("apple","orange","cherry")
+#print(x)
+
+#tup=(1,2,3,4)
+#tup[1]=3
+#print(tup[1])
+
+##slicing
+
+##mytub=(1,2,3,4,5)
+#a=mytub[1:3]
+#print(a) 
+
+#concatination
+
+#mytup1=("h")
+#mytup2=("i")
+#mytup1+=mytup2
+#print(mytup1)
+
+#repeatation
+
+#mytup=(1,2,3,4,5)
+#a=mytup*2
+#print(a)
+
+##range
+
+#x=range(6)
+#print(type(x))
+
+
+#a=range(0,14,3)
+#b=list(a)
+#print(b)
+
+#a=range(8,1,-1)
+#b=list(a)
+#print(b)
+
+#ex1
+
+#a=int(input("Enter 1st num:"))
+#b=int(input("Enter 2nd num:"))
+#result=a*b
+#print("Result:",result)
+
+#ex2
+
+#print("my","name","Is","james",sep="**")
+
+#ex3
+#num=8
+#print("%o" % num)
+
+#ex4
+#num=458.541315
+#print("%.3f" % num)
+
+#ex5
+#list=[]
+#for i in range(0,5):
+#   print("Enter number at location:",i)
+#   item=float(input())
+#   list.append(item)
+
+#print("user input:",list)   
+
+
+#or i in range(1,5):
+
+#   print(i)
+
+str1="ROHIT"
+str2=" SHARMA"
+str1+=str2
+print(str1)
